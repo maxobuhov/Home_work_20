6439c2611bed864532e9ef54301bb30b59a632bc
Охрана <maxfaktor1991@yandex.ru>
2023-10-12 15:09:54 +0300

    feat: Функция приветствия теперь принимает аргумент имя

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def say_hello():
-    print('Hello world')
+def say_hello(name: str):
+    print(f'Hello {name}')
 
 
 def main():
+    name = input('Enter your name: ')
     say_hello()
+    input('Press Enter to continue ...')
+
+
+if __name__ == '__main__':
+    main()
